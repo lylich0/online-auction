952587ebd989ffa475161e3971b0b982c195c4a6
lylich0 <bibbbik@gmail.com>
2024-02-14 01:12:19 +0200

    [add] cards dynamic loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,32 @@ export class AppComponent {
       title: "Card 4",
       content: "Some quick example to show off the card's 4 content.",
       imageUrl: "assets/card-images/card-img-4.png"
-    }
+    },
+    {
+      title: "Card 1",
+      content: "Some quick example to show off the card's 1 content.",
+      imageUrl: "assets/card-images/card-img-1.png"
+    },
+    {
+      title: "Card 2",
+      content: "Some quick example to show off the card's 2 content.",
+      imageUrl: "assets/card-images/card-img-2.png"
+    },
+    {
+      title: "Card 3",
+      content: "Some quick example to show off the card's 3 content.",
+      imageUrl: "assets/card-images/card-img-3.png"
+    },
+    {
+      title: "Card 4",
+      content: "Some quick example to show off the card's 4 content.",
+      imageUrl: "assets/card-images/card-img-4.png"
+    },
+    {
+      title: "Card 1",
+      content: "Some quick example to show off the card's 1 content.",
+      imageUrl: "assets/card-images/card-img-1.png"
+    },
   ];
 
 }
