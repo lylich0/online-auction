0d7770a06fc78b1375cba14f6c805a92bb5149be
lylich0 <bibbbik@gmail.com>
2024-03-09 08:43:24 +0200

    [add] persistent auth state via Firebase

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { AuthService } from "../../services/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+
   constructor(private authService: AuthService) {}
 
-  isLoggedIn(): boolean {
-    return this.authService.isLoggedIn;
+  ngOnInit() {
+    this.authService.authInitialized$.subscribe(initialized => {
+      if (initialized) {
+        this.authService.isLoggedIn$.subscribe(isLoggedIn => {
+          this.isLoggedIn = isLoggedIn;
+        });
+      }
+    });
   }
 
-  logout(): void {
-    this.authService.logout();
+  onLogout() {
+    this.authService.signout().subscribe(() => {
+      this.isLoggedIn = false;
+    });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,91 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject, catchError, from, switchMap, tap} from "rxjs";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn = false;
+  private _authInitialized$ = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = new BehaviorSubject<boolean>(false);
 
-  constructor() {}
-
-  login() {
-    this.isLoggedIn = true;
+  constructor(private afAuth: AngularFireAuth) {
+    this.setPersistence().then(() => {
+      this.handleAuthState();
+      this._authInitialized$.next(true);
+    });
   }
 
-  logout() {
-    this.isLoggedIn = false;
+  async setPersistence() {
+    await this.afAuth.setPersistence('session');
+  }
+
+  handleAuthState() {
+    this.afAuth.authState.pipe(
+      tap(user => {
+        const isLoggedIn = !!user;
+        this.isLoggedIn$.next(isLoggedIn);
+      })
+    ).subscribe();
+  }
+
+  get authInitialized$() {
+    return this._authInitialized$.asObservable();
+  }
+
+  signup(email: string, password: string) {
+    return from(this.afAuth.createUserWithEmailAndPassword(email, password)).pipe(
+      switchMap(() => {
+        this.isLoggedIn$.next(true);
+        return this.isLoggedIn$;
+      }),
+      catchError(error => {
+        console.error('Signup error:', error.message);
+        throw error;
+      })
+    );
+  }
+
+  signin(email: string, password: string) {
+    return from(this.afAuth.signInWithEmailAndPassword(email, password)).pipe(
+      switchMap(() => {
+        this.isLoggedIn$.next(true);
+        return this.isLoggedIn$;
+      }),
+      catchError(error => {
+        console.error('Login error:', error.message);
+        throw error;
+      })
+    );
+  }
+
+  signout() {
+    return from(this.afAuth.signOut()).pipe(
+      switchMap(() => {
+        this.isLoggedIn$.next(false);
+        return this.isLoggedIn$;
+      }),
+      catchError(error => {
+        console.error('Logout error:', error.message);
+        throw error;
+      })
+    );
+  }
+
+  private getErrorMessage(error: any): string {
+    switch (error.code) {
+      case 'auth/wrong-password':
+        return 'Password is incorrect';
+      case 'auth/user-not-found':
+        return "Account with provided email doesn't exist";
+      case 'auth/email-already-in-use':
+        return 'Email address is already in use';
+      case 'auth/weak-password':
+        return 'Password should be at least 6 characters';
+      case 'auth/invalid-email':
+        return 'Invalid email address';
+      default:
+        return 'An error occurred while signing up. Please try again later';
+    }
   }
 }
